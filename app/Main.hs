68783028cc64318d710455a8274be86118305333
lykmast <lykmast@gmail.com>
2023-10-14 13:21:59 +0300

    Handle EOF in repl.

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 import PPrint(pprint)
 import Eval (eval)
 import Parser(theParser)
-import GHC.IO.Handle (hFlush)
+import GHC.IO.Handle (hFlush, isEOF)
 import System.IO (stdout)
 import Typecheck(typecheckTop)
 import Environment(Environment, envInsert, emptyEnv)
@@ -14,26 +14,27 @@ main :: IO ()
 main = repl emptyEnv
 
 repl :: Environment -> IO ()
-repl env = do 
+repl env = do
   putStr "λ> "
   hFlush stdout
-  inp <- getLine
-  case theParser inp of
-    Left e -> do
-      putStrLn "Parse error:"
-      print e
-      repl env
-    Right top ->
-      case handleTop top of
-        Left e -> do
-          putStrLn e
-          repl env
-        Right (ty, res, newEnv) -> do
-          pprint res
-          putStr "\n:: "
-          pprint ty
-          putStrLn ""
-          repl newEnv
+  unlessEOF $ do
+    inp <- getLine
+    case theParser inp of
+      Left e -> do
+        putStrLn "Parse error:"
+        print e
+        repl env
+      Right top ->
+        case handleTop top of
+          Left e -> do
+            putStrLn e
+            repl env
+          Right (ty, res, newEnv) -> do
+            pprint res
+            putStr "\n:: "
+            pprint ty
+            putStrLn ""
+            repl newEnv
   where
     handleTop :: TopLevel -> Either String (Type, Term, Environment)
     handleTop (Left t) = do
@@ -44,3 +45,9 @@ repl env = do
       ty  <- first ("Type error:\n" ++ ) $ typecheckTop env t 
       res <- first ("Eval error:\n" ++ ) $ eval env t
       return (ty, res, envInsert x (ty, res) env)
+
+    unlessEOF :: IO () -> IO ()
+    unlessEOF act = do
+      done <- isEOF
+      if done then putStrLn "\nOk bye."
+      else act
